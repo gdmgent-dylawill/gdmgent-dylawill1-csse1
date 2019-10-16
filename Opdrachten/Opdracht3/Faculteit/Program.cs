117339db3c7c878bfba4e793e3deb358b16df3d9
gdmgent-dylawill <dylan.willems@student.arteveldehs.be>
2019-10-17 00:51:00 +0200

    *exercises*

---
+++ Opdrachten/Opdracht3/Faculteit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Oefening4 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("-- Oef 4: Faculteit --");
+
+            // Declaratie van de variabelen
+            // Faculteit van 0 = 1
+            int Counter, Number, Faculteit;
+            Faculteit = 1;
+
+            // Opvragen van een willekeurig getal
+            Console.WriteLine("Geef een willekeurig getal in");
+            Number = Convert.ToInt32(Console.ReadLine());
+
+            // Berekening van de faculteit van ingegeven getal
+            for (Counter = 1; Counter <= Number; Counter++) {
+                Faculteit = Faculteit * Counter;
+            }
+
+            // Weergave in de console
+            Console.WriteLine("De faculteit van " + Number + " is " + Faculteit);
+        }
+    }
+};
---
+++ Opdrachten/Opdracht3/Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Oefening5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-- Oef 5: Fibonacci --");
+
+            // Declaratie van de variabelen
+            int Length, Counter, Fibo1, Fibo2, Fibo3;
+            Fibo1 = 0;
+            Fibo2 = 1;
+
+            // Lengte reeks opvragen
+            Console.WriteLine("Geef het aantal elementen in:");
+            Length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("De lengte van de reeks is: " + Length);
+            Length = Length - 3;
+
+
+            // De reeks laten starten met de eerste 2 getallen van Fibonacci (0 en 1)
+            Console.WriteLine(Fibo1);
+            Console.WriteLine(Fibo2);
+
+            // Weergeven van de reeks van Fibonacci (met ingegeven aantal elementen)
+            for(Counter = 0; Counter <= Length; Counter++) {
+                Fibo3 = Fibo1 + Fibo2;
+                Fibo1 = Fibo2;
+                Fibo2 = Fibo3;
+
+                // Weergave in de console
+                Console.WriteLine(Fibo3);
+            }
+        }
+    }
+};
