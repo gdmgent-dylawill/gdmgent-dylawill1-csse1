b7942e2da2fc3016e124ad54698e7f393170066b
gdmgent-dylawill <dylan.willems@student.arteveldehs.be>
2019-11-21 00:21:23 +0100

    updated 'Lessen' folder

---
+++ Lessen/Les6/Oefening1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Les6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /*
+            try
+            {
+                // Code die je wilt uitoveren …
+                throw new Exception();
+            }
+            catch (System.Exception)
+            {
+                // Opvangen van de exception
+                throw;
+            }
+            */
+
+            object o = null;  
+            try  
+            {  
+                int i = (int)o;   // Error  
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("NRE: " + e.Message);
+                Console.WriteLine("NRE: " + e.StackTrace); // StackTrace zegt op welke lijn het probleem zich voordoet
+            }
+            catch (InvalidCastException e)   
+            {  
+                Console.WriteLine("ICE: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("E: " + e.StackTrace);
+            }
+            finally
+            {
+                // Stappen die zeker nog moeten uitgevoerd worden ...
+                Console.WriteLine("Finally");
+            }
+        }
+        
+        
+    }
+}
